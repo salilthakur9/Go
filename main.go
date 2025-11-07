2d5c0273e06d93f32dde80868a0ce9777a654c80
Salil Thakur <salilthakur09@gmail.com>
2025-11-07 18:45:32 +0530

    basic maths, updated Readme

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
-package main
+/*package main
 import "fmt"
 
 func main(){
 	var a="salil"
 	fmt.Println(a)
-}
+}*/
+
+
+
+/*package main
+import "fmt"
+func main(){
+	var a uint16=200
+	fmt.Println(a);
+}*/
+
+
+/*package main
+import "fmt"
+func main(){
+	var a int32=30;
+	var b int32=20;
+	fmt.Printf("Addition: %d+%d=%d", a, b, a+b);
+	fmt.Println();
+	fmt.Printf("Subtraction: %d-%d=%d\n", a, b, a-b);
+	fmt.Printf("Multiplication: %d*%d=%d\n", a, b, a*b);
+	fmt.Printf("Divide: %d/%d=%d", a,b,a/b);
+	fmt.Println();
+	fmt.Printf("Remainder: %d %% %d=%d", a,b,a%b);
+}*/
